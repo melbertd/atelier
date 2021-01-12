52c6d163bffba114475652b0267f7e03a74aa074
melbertd <melbert.s.dela.paz@accenture.com>
2021-01-12 14:51:26 +0800

    fix deploy issues

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
       <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Andrea Suarez Atelier</a>
+          <a href="/home" className="navbar-brand" href="#">Andrea Suarez Atelier</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
